09de4c843c8231206ace920f20c9cd42951fee8e
Hiroki Ueno <rockyueno@gmail.com>
2024-10-11 15:03:26 -0700

    ex11

--- dist/ex11.js
+++ dist/ex11.js
@@ -1,31 +1,21 @@
+"use strict";
 // Create two interfaces `Flyable` and `Swimmable`. Each should have unique methods.
 // Create a class `Duck` that implements both interfaces.
 // Write a function `getCapabilities` that accepts an object with both interfaces and logs the duck's capabilities.
 // Return type: void.
-
-interface Flyable {
-  fly(): void;
+class Duck {
+    fly() {
+        console.log("Duck is flying");
+    }
+    swim() {
+        console.log("Duck is swimming");
+    }
 }
-
-interface Swimmable {
-  swim(): void;
+function getCapabilities(duck) {
+    duck.fly();
+    duck.swim();
 }
-
-class Duck implements Flyable, Swimmable {
-  fly() {
-      console.log("Duck is flying");
-  }
-  swim() {
-      console.log("Duck is swimming");
-  }
-}
-
-function getCapabilities(duck: Duck): void {
-  duck.fly();
-  duck.swim();
-}
-
 // Expected output:
-getCapabilities(new Duck())
+getCapabilities(new Duck());
 // Duck is flying
 // Duck is swimming
